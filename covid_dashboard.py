b0693dc70c669a38caf9465db08f3866c17c2b5f
maxwhyte <Fucker999>
2021-10-19 22:42:44 +0200

    Updates source for csv

--- covid_dashboard.py
+++ covid_dashboard.py
@@ -1,10 +1,10 @@
 ### Covid-19 Dashboard
 
+#Imports neccessary packages
 import streamlit as st
 import pandas as pd
-import plotly_express as px
 import plotly.graph_objects as go
-
+import locale
 
 #Title and header
 st.title("Covid-19 in Numbers")
@@ -15,8 +15,8 @@ Data Analysis for Covid-19 related numbers: cases and deaths by country and worl
 
 #Data import and columns selection
 #Case and Death numbers
-data = "https://covid.ourworldindata.org/data/ecdc/full_data.csv"
-df = st.cache(pd.read_csv)(data)  # caches DF, @st.cache for functions
+data = "https://covid.ourworldindata.org/data/owid-covid-data.csv"
+df = st.cache(pd.read_csv)(data, sep=",")  # caches DF, @st.cache for functions
 
 # Country Selection:
 countries = list(df["location"].unique()) #creating a list with countries
@@ -31,31 +31,34 @@ df_filtered = df[df["location"] == country]
 latest_deathcount = df_filtered.iloc[-1:,5]
 latest_casecount = df_filtered.iloc[-1:,4]
 
-
-st.write(country + "  -  Confirmed Cases: " + str(latest_casecount.values) + "  -  Confirmed Deaths: "
-		 + str(latest_deathcount.values))
-
+st.write(country + "  -  Confirmed Cases: " + str(int(latest_casecount)) + "  -  Confirmed Deaths: "
+		 + str(int(latest_deathcount)))
 
 # Create Bar charts using plotly
-fig_deaths = go.Figure(data=[
-	go.Bar(name="Total Deaths", x=df_filtered.date, y=df_filtered['total_deaths'],
- 				),
- 	go.Bar(name="New Deaths", x=df_filtered.date, y=df_filtered['new_deaths']
-     )])
-fig_cases = go.Figure(data=[
-	go.Bar(name="Total Cases", x=df_filtered.date, y=df_filtered['total_cases']
- 				),
+fig_new_cases = go.Figure(data=[
  	go.Bar(name="New Cases", x=df_filtered.date, y=df_filtered['new_cases']
-                      )
-	])
+    )])
+fig_new_deaths = go.Figure(data=[
+ 	go.Bar(name="New Deaths", x=df_filtered.date, y=df_filtered['new_deaths']
+    )])
+fig_total_cases = go.Figure(data=[
+ 	go.Bar(name="Total Cases", x=df_filtered.date, y=df_filtered['total_cases']
+	)])
+fig_total_deaths = go.Figure(data=[
+	go.Bar(name="Total Deaths", x=df_filtered.date, y=df_filtered['total_deaths']
+    )])
 
 # Customize aspect
-fig_cases.update_layout(title_text='Cases in ' + country, xaxis_rangeslider_visible=True)
-fig_deaths.update_layout(title_text='Deaths in ' + country, xaxis_rangeslider_visible=True)
+fig_new_cases.update_layout(title_text='New Cases in ' + country, xaxis_rangeslider_visible=True)
+fig_new_deaths.update_layout(title_text='New Deaths in ' + country, xaxis_rangeslider_visible=True)
+fig_total_cases.update_layout(title_text='Cum. Cases in ' + country, xaxis_rangeslider_visible=True)
+fig_total_deaths.update_layout(title_text='Cum. Deaths in ' + country, xaxis_rangeslider_visible=True)
 
 # Plot the 4 different charts
-st.plotly_chart(fig_cases)
-st.plotly_chart(fig_deaths)
+st.plotly_chart(fig_new_cases)
+st.plotly_chart(fig_new_deaths)
+st.plotly_chart(fig_total_cases)
+st.plotly_chart(fig_total_deaths)
 
 
 # About me
